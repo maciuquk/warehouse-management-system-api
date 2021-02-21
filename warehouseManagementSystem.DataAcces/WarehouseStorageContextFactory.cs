64aa6f5c289b05e229d56d6a4445552c847b6a19
maciuquk <maciejromik@gmai.com>
2021-02-21 10:06:52 +0100

    Add dbContext

---
+++ warehouseManagementSystem.DataAcces/WarehouseStorageContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace warehouseManagementSystem.DataAcces
+{
+    public class WarehouseStorageContextFactory : IDesignTimeDbContextFactory<WarehouseStorageContext>
+    {
+        public WarehouseStorageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WarehouseStorageContext>();
+            optionsBuilder.UseSqlServer();
+            return new WarehouseStorageContext(optionsBuilder.Options);
+        }
+    }
+}
